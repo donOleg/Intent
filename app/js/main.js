3af8305ec67ad794a1b3c798d34be197abf4acac
donOleg <O.Petrovich.S@yandex.ua>
2016-03-01 13:47:19 +0200

    Edit content

--- app/js/main.js
+++ app/js/main.js
@@ -4,4 +4,10 @@ $(document).ready(function() {
     pagerCustom: '#bx-pager'
   });
 
+  $(".container_tabs .tabs .tab").click(function(){
+    $(".container_tabs .tabs .tab").removeClass("active").eq($(this).index()).addClass("active");
+    $(".container_tabs .nav_item").hide().eq($(this).index()).fadeIn()
+  }).eq(0).addClass("active");
+
+
 })
